75fec022cb2a832b128366a4cf4c37961b615c65
ABO24002278 - Erik Abolins <ABO24002278@HeartofYorkshire.ac.uk>
2024-11-21 16:10:00 +0000

    .

--- 2DPlatformer/Assets/Scripts/MainMenu.cs
+++ 2DPlatformer/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     public string Optname;
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
     public void Optionsmenu()
     {
