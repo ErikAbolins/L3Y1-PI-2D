fe3af702f2cc56ad4aa1eea3ed7e1708388893fc
ABO24002278 - Erik Abolins <ABO24002278@HeartofYorkshire.ac.uk>
2024-12-03 15:46:29 +0000

    Pause Menu

---
+++ 2DPlatformer/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    public static bool gameIsPaused;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            Pause();
+        }
+    }
+    
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    
+    public void QuitToMain()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+}
